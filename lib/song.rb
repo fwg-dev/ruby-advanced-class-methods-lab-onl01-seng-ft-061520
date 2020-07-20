e1a50dc0c8ec711275d048614ef7bd2e7b819466
Faith  Gicheha <fwgicheh@gmail.com>
2020-07-20 23:56:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,24 @@
 class Song
 
+attr_accessor :name, :artist_name
+@@all =[]
+# def initialize(name, artist_name)
+#   @name = name 
+#   @artist_name = artist_name
+#   @@all << self 
+#   save 
+# end 
+
+def self.all 
+  @@all 
+end 
+def save 
+  self.class.all << self 
+end 
+
+def self.create
+  song = Song.new
+  song.save
+  song
+end 
 end
